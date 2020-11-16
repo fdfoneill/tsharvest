d61aecb10e3ff82890f85b9f0ce28bfee5e97273
fdfoneill <fdfoneill@gmail.com>
2020-11-16 16:14:47 -0500

    Copy errors

--- tsharvest/zonal.py
+++ tsharvest/zonal.py
@@ -39,8 +39,8 @@ def _zonal_worker(args):
 
 	# get shape raster info
 	shape_handle = rasterio.open(shape_path,'r')
-	shape_noDataVal = mask_handle.meta['nodata']
-	shape_data = mask_handle.read(1,window=targetwindow)
+	shape_noDataVal = shape_handle.meta['nodata']
+	shape_data = shape_handle.read(1,window=targetwindow)
 	shape_handle.close()
 
 
@@ -49,13 +49,13 @@ def _zonal_worker(args):
 
 	# loop over all admin codes present in shape_
 	uniquezones = np.unique(shape_data[shape_data != shape_noDataVal]) # exclude nodata value
-	for zone_code in uniqueadmins:
+	for zone_code in uniquezones:
 		valid_pixels = int((shape_data[(shape_data == zone_code)]).size)
-		if arable_pixels == 0:
+		if valid_pixels == 0:
 			continue
 		masked = np.array(product_data[(product_data != product_noDataVal) & (shape_data == zone_code)], dtype='int64')
 		value = (masked.mean() if (masked.size > 0) else 0)
-		out_dict[admin_code] = {"value":value,"pixels":valid_pixels}
+		out_dict[zone_code] = {"value":value,"pixels":valid_pixels}
 
 	return out_dict
 
@@ -80,13 +80,13 @@ def _update(stored_dict,this_dict) -> dict:
 			continue
 		try:
 			# weight of stored_dict value is the ratio of its valid pixels to the total new sum of valid pixels
-			stored_weight = stored_info['pixels'] / (stored_info['pixels'] + this_info['pixels'])
+			stored_weight = float(stored_info['pixels']) / float(stored_info['pixels'] + this_info['pixels'])
 		except ZeroDivisionError:
 			# if no valid pixels at all, weight everything at 0
 			stored_weight = 0
 		try:
 			# weight of this_dict value is the ratio of its valid pixels to the total new sum of valid pixels
-			this_weight = this_info['pixels'] / (stored_info['pixels'] + this_info['pixels'])
+			this_weight = float(this_info['pixels']) / float(stored_info['pixels'] + this_info['pixels'])
 		except ZeroDivisionError:
 			# if the total valid pixels are 0, everything gets weight 0
 			this_weight = 0
