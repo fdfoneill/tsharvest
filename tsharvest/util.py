616fb6a7da36058c3fdf064dbf33738d771c0c1c
fdfoneill <fdfoneill@gmail.com>
2020-11-17 15:36:08 -0500

    Attempt to catch dtype overflow

--- tsharvest/util.py
+++ tsharvest/util.py
@@ -152,7 +152,11 @@ def shapefile_toRaster(shapefile_path, model_raster, out_path, zone_field:str = 
 		meta.update(dtype="int32")
 	#meta.update(compress='packbits')
 
-	with rasterio.open(out_path, 'w+', **meta) as out:
+	try:
+		out =  rasterio.open(out_path, 'w+', **meta)
+	except ValueError:
+		meta.update(dtype=rasterio.dtypes.get_minimum_dtype([meta['nodata']]))
+		out = rasterio.open(out_path, 'w+', **meta)
 		out_arr = out.read(1)
 
 		# this is where we create a generator of geom, value pairs to use in rasterizing
@@ -166,6 +170,7 @@ def shapefile_toRaster(shapefile_path, model_raster, out_path, zone_field:str = 
 
 		burned = features.rasterize(shapes=shapes, fill=0, out=out_arr, transform=out.transform)
 		out.write_band(1, burned)
+	out.close()
 
 	return out_path
 
